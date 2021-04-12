9cfb325819017ec1ef92b750d944127ddf318431
jelgar <jamesnelgar@gmail.com>
2021-04-12 21:45:36 +0100

    Basic webrtc setup

---
+++ webrtc.js
@@ -0,0 +1,24 @@
+console.log("Hows it going Joe");
+
+const constraints = {
+    'video': true,
+    'audio': true
+}
+navigator.mediaDevices.getUserMedia(constraints)
+    .then(stream => {
+        console.log('Got MediaStream:', stream);
+    })
+    .catch(error => {
+        console.error('Error accessing media devices.', error);
+    });
+
+async function playVideoFromCamera() {
+    try {
+        const constraints = {'video': true, 'audio': true};
+        const stream = await navigator.mediaDevices.getUserMedia(constraints);
+        const videoElement = document.querySelector('video#localVideo');
+        videoElement.srcObject = stream;
+    } catch(error) {
+        console.error('Error opening video camera.', error);
+    }
+}
